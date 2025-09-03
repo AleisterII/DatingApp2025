046a5e30a4f9a8e23a7d341f646dc54b5552bd69
AleisterII <118698761+AleisterII@users.noreply.github.com>
2025-09-02 19:26:51 -0600

    parcial01 - client creation
    Implementacion de funciones asincronas y creacion del cliente en angular

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -2,25 +2,26 @@ using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")] // localhost:5001/api/members
+    [Route("api/[controller]")] // https://localhost:5001/api/members
     [ApiController]
     public class MembersController(AppDbContext context) : ControllerBase
     {
         [HttpGet]
-        public ActionResult<IReadOnlyList<AppUser>> GetMembers()
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
         {
-            var members = context.Users.ToList();
+            var members = await context.Users.ToListAsync();
 
             return members;
         }
 
-        [HttpGet("{id}")] // https://localhostr:5001/api/members/bob-id
-        public ActionResult<AppUser> GetMember(string id)
+        [HttpGet("{id}")] // https://localhost:5001/api/members/bob-id
+        public async Task<ActionResult<AppUser>> GetMember(string id)
         {
-            var member = context.Users.Find(id);
+            var member = await context.Users.FindAsync(id);
             // var member2 = context.Users.FirstOrDefault(x => x.Id == id);
             if (member == null) return NotFound();
 
