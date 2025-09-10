a69d68db0ff35237d7bc77b41a6c494a6d92efda
AleisterII <118698761+AleisterII@users.noreply.github.com>
2025-09-10 07:24:58 -0600

    Parcial02 - Add API controller

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -4,28 +4,25 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Controllers
+namespace API.Controllers;
+public class MembersController(AppDbContext context) : BaseApiController
 {
-    [Route("api/[controller]")] // https://localhost:5001/api/members
-    [ApiController]
-    public class MembersController(AppDbContext context) : ControllerBase
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
     {
-        [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
-        {
-            var members = await context.Users.ToListAsync();
+        var members = await context.Users.ToListAsync();
 
-            return members;
-        }
+        return members;
+    }
 
-        [HttpGet("{id}")] // https://localhost:5001/api/members/bob-id
-        public async Task<ActionResult<AppUser>> GetMember(string id)
-        {
-            var member = await context.Users.FindAsync(id);
-            // var member2 = context.Users.FirstOrDefault(x => x.Id == id);
-            if (member == null) return NotFound();
+    [HttpGet("{id}")] // https://localhost:5001/api/members/bob-id
+    public async Task<ActionResult<AppUser>> GetMember(string id)
+    {
+        var member = await context.Users.FindAsync(id);
+        // var member2 = context.Users.FirstOrDefault(x => x.Id == id);
+        if (member == null) return NotFound();
 
-            return member;
-        }
+        return member;
     }
 }
+
