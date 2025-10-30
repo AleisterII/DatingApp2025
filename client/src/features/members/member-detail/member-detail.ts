10767834a5ee5e3d2020af63660b4ad4a85c0d6d
AleisterII <118698761+AleisterII@users.noreply.github.com>
2025-10-30 07:22:30 -0600

    Parcial03 - Add Profile details

--- client/src/features/members/member-detail/member-detail.ts
+++ client/src/features/members/member-detail/member-detail.ts
@@ -1,13 +1,12 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { MembersService } from '../../../core/services/members-service';
 import { ActivatedRoute, NavigationEnd, Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { AsyncPipe } from '@angular/common';
 import { Member } from '../../../types/member';
-import { filter, Observable } from 'rxjs';
+import { filter} from 'rxjs';
 
 @Component({
   selector: 'app-member-detail',
-  imports: [AsyncPipe, RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [ RouterLink, RouterLinkActive, RouterOutlet],
   templateUrl: './member-detail.html',
   styleUrl: './member-detail.css'
 })
