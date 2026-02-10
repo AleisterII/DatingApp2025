7927bf32125ea9f4e8a26b45178e9ba40eee9f5d
AleisterII <118698761+AleisterII@users.noreply.github.com>
2026-02-10 07:42:00 -0600

    Parcial04 - Add edit profile button

--- client/src/features/member-profile/member-profile.ts
+++ client/src/features/member-profile/member-profile.ts
@@ -1,7 +1,8 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../types/member';
 import { DatePipe } from '@angular/common';
+import { MembersService } from '../../core/services/members-service';
 
 @Component({
   selector: 'app-member-profile',
@@ -9,14 +10,14 @@ import { DatePipe } from '@angular/common';
   templateUrl: './member-profile.html',
   styleUrl: './member-profile.css'
 })
-export class MemberProfile {
+export class MemberProfile implements OnInit {
   private route = inject(ActivatedRoute);
-  protected member = signal<Member | undefined> (undefined);
+  protected member = signal<Member | undefined>(undefined);
+  protected membersService = inject(MembersService);
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe(data => {
-      this.member.set(data["member"])
+      this.member.set(data["member"]);
     })
   }
-
 }
