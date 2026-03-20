24918b514b49d6eb00d5c51a8fd8133be5f7dfe4
AleisterII <118698761+AleisterII@users.noreply.github.com>
2026-03-19 20:47:28 -0600

    Pacial05 - Add filter modal 1/2

--- client/src/features/members/filter-modal/filter-modal.ts
+++ client/src/features/members/filter-modal/filter-modal.ts
@@ -1,11 +1,28 @@
-import { Component, ElementRef, OnInit, viewChild } from '@angular/core';
+import { Component, ElementRef, output, ViewChild, viewChild } from '@angular/core';
+import { MemberParams } from '../../../types/member';
 
 @Component({
   selector: 'app-filter-modal',
   imports: [],
   templateUrl: './filter-modal.html',
-  styleUrl: './filter-modal.css',
+  styleUrl: './filter-modal.css'
 })
 export class FilterModal {
+  @ViewChild('filterModal') modalRef!: ElementRef<HTMLDialogElement>;
+  closeModal = output();
+  submitData = output<MemberParams>();
 
+  open() {
+    this.modalRef.nativeElement.showModal();
+  }
+
+  close() {
+    this.modalRef.nativeElement.close();
+    this.closeModal.emit();
+  }
+
+  submit() {
+    this.submitData.emit(new MemberParams());
+    this.close();
+  }
 }
