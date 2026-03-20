072ec2d04ef86417771191ba8d6c48c53d1de97f
AleisterII <118698761+AleisterII@users.noreply.github.com>
2026-03-19 20:37:36 -0600

    Pacial05 - Add more filtering to API

---
+++ client/src/features/members/filter-modal/filter-modal.ts
@@ -0,0 +1,11 @@
+import { Component, ElementRef, OnInit, viewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-modal',
+  imports: [],
+  templateUrl: './filter-modal.html',
+  styleUrl: './filter-modal.css',
+})
+export class FilterModal {
+
+}
