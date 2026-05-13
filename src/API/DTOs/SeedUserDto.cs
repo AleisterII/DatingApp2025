15f7e4aac6b5386a40287323c246977286c0ca7a
AleisterII <118698761+AleisterII@users.noreply.github.com>
2026-05-13 07:34:10 -0600

    Parcial06 - IDENTITY - Setup Identity

--- src/API/DTOs/SeedUserDto.cs
+++ src/API/DTOs/SeedUserDto.cs
@@ -1,6 +1,6 @@
 namespace API.DTOs;
 
-public class SeedUsersDto
+public class SeedUserDto
 {
     public required string Id { get; set; }
     public required string Email { get; set; }
---
+++ src/API/Data/Migrations/20260513132545_IdentityAdded.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "admin-id",
+                column: "ConcurrencyStamp",
+                value: "9865050e-6d17-4604-bbad-ac023c682e52");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "member-id",
+                column: "ConcurrencyStamp",
+                value: "3acf1487-6afa-4a5a-84d4-48a0610c66ac");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "moderator-id",
+                column: "ConcurrencyStamp",
+                value: "a66b467e-5bc8-4477-8f65-ab030132620a");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "admin-id",
+                column: "ConcurrencyStamp",
+                value: "8ada9622-e2d1-43a7-b766-0bea5b0af243");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "member-id",
+                column: "ConcurrencyStamp",
+                value: "614141aa-6201-4b34-a88d-c3a5cb398242");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "moderator-id",
+                column: "ConcurrencyStamp",
+                value: "dfcfa935-bba2-495c-8fcb-ae6c673f797c");
+        }
+    }
+}
