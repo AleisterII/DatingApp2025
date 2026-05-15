f7c329f23ee48059465c4e6dcebec67f708b487d
AleisterII <118698761+AleisterII@users.noreply.github.com>
2026-05-14 21:36:11 -0600

    Parcial06 - IDENTITY - Fix breaking change in Identity

---
+++ src/API/Data/Migrations/20260515033600_IdentityUpdated.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class IdentityUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "admin-id",
+                column: "ConcurrencyStamp",
+                value: "eb15ffa3-d9b0-41a1-a14e-8a870a73ffd4");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "member-id",
+                column: "ConcurrencyStamp",
+                value: "31b97392-f0d6-4898-8904-bc302a6eff9b");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "moderator-id",
+                column: "ConcurrencyStamp",
+                value: "1b8bf25e-41d4-420c-98aa-9c8287d310bf");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "admin-id",
+                column: "ConcurrencyStamp",
+                value: "4ce0edbd-e3a4-49be-9572-58d6b23da8c7");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "member-id",
+                column: "ConcurrencyStamp",
+                value: "7a6818dc-6840-4deb-ad4d-dbebf52f6ddd");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "moderator-id",
+                column: "ConcurrencyStamp",
+                value: "4fe9abb1-d7c7-46fb-89a7-921c901240dd");
+        }
+    }
+}
