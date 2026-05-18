63500bc5d5f20e5ee4f86dfa56394113f18925b2
AleisterII <118698761+AleisterII@users.noreply.github.com>
2026-05-18 15:26:52 -0600

    Parcial06 - SignalR - Add presence hub

--- src/API/SignalR/PresenceHub.cs
+++ src/API/SignalR/PresenceHub.cs
@@ -4,10 +4,18 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace API.SignalR;
 
+[Authorize]
 public class PresenceHub : Hub
 {
     public override async Task OnConnectedAsync()
     {
-        await Clients.Others.SendAsync("UserIsOnline", Context.User?.Claims.FirstOrDefault());
+        await Clients.Others.SendAsync("UserOnline", Context.User?.FindFirstValue(ClaimTypes.Email));
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        await Clients.Others.SendAsync("UserOffline", Context.User?.FindFirstValue(ClaimTypes.Email));
+        
+        await base.OnDisconnectedAsync(exception);
     }
 }
